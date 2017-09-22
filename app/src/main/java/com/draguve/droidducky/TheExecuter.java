4d5fcf8c4e86347a47c700dda0989b7448c00808
draguve <ritwikpatil1998@gmail.com>
2017-09-23 02:58:01 +0530

    Executer Now Works , Phone Works As Keyboard
    Seperated the copying code from the main
    Added a class to execute stuff as root
    Prints test to the computer
    
    To Fix:
       Timing issues
       Make more optimized
       Try exectuting keystokes without root

---
+++ app/src/main/java/com/draguve/droidducky/TheExecuter.java
@@ -0,0 +1,71 @@
+package com.draguve.droidducky;
+
+import android.util.Log;
+
+import java.io.BufferedReader;
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+
+/**
+ * Created by Draguve on 9/23/2017.
+ */
+
+public class TheExecuter {
+
+    private final static String TAG = "TheExecutor";
+
+    public TheExecuter() {
+
+    }
+
+    public static void RunAsRoot(String[] command) {
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            DataOutputStream os = new DataOutputStream(process.getOutputStream());
+            for (String tmpmd : command) {
+                os.writeBytes(tmpmd + '\n');
+            }
+            os.writeBytes("exit\n");
+            os.flush();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public static String RunAsRootOutput(String command) {
+        String output = "";
+        String line;
+        try {
+            Process process = Runtime.getRuntime().exec("su");
+            OutputStream stdin = process.getOutputStream();
+            InputStream stderr = process.getErrorStream();
+            InputStream stdout = process.getInputStream();
+
+            stdin.write((command + '\n').getBytes());
+            stdin.write(("exit\n").getBytes());
+            stdin.flush();
+            stdin.close();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(stdout));
+            while ((line = br.readLine()) != null) {
+                output = output + line;
+            }
+            br.close();
+            br = new BufferedReader(new InputStreamReader(stderr));
+            while ((line = br.readLine()) != null) {
+                Log.e("Shell Error:", line);
+            }
+            br.close();
+            process.waitFor();
+            process.destroy();
+        } catch (IOException e) {
+            Log.d(TAG, "An IOException was caught: " + e.getMessage());
+        } catch (InterruptedException ex) {
+            Log.d(TAG, "An InterruptedException was caught: " + ex.getMessage());
+        }
+        return output;
+    }
+}
