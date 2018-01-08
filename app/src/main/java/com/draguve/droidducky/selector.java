6132bf244906fb16fecda1faea2b632171f96de8
Draguve <ritwikpatil1998@gmail.com>
2018-01-08 23:08:44 +0530

    open different app for getting script

--- app/src/main/java/com/draguve/droidducky/selector.java
+++ app/src/main/java/com/draguve/droidducky/selector.java
@@ -1,8 +1,10 @@
 package com.draguve.droidducky;
 
 import android.Manifest;
+import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.os.Bundle;
 import android.os.Environment;
 import android.support.v4.app.ActivityCompat;
@@ -22,7 +24,10 @@ import android.widget.ArrayAdapter;
 import android.widget.Spinner;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -33,6 +38,9 @@ public class selector extends AppCompatActivity {
     private RecyclerView recyclerView;
     private ScriptsAdapter mAdapter;
     public static final int MULTIPLE_PERMISSIONS = 10; // code you want.
+
+    static final int OPEN_WRITER = 1;
+    static final int FIND_FILE = 1337;
 
     String[] permissions= new String[]{
             Manifest.permission.WRITE_EXTERNAL_STORAGE,
@@ -76,6 +84,28 @@ public class selector extends AppCompatActivity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode==FIND_FILE){
+            Uri uri = null;
+            if (data != null) {
+                uri = data.getData();
+                InputStream inputStream = null;
+                try {
+                    inputStream = getContentResolver().openInputStream(uri);
+                    BufferedReader reader = new BufferedReader(new InputStreamReader(
+                            inputStream));
+
+                    String line;
+                    Log.i("","open text file - content"+"\n");
+                    while ((line = reader.readLine()) != null) {
+                        Log.i("",line+"\n");
+                    }
+                    reader.close();
+                    inputStream.close();
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+            }
+        }
         scriptList = db.getAllScripts();
         mAdapter.updateScriptList(scriptList);
     }
@@ -114,10 +144,9 @@ public class selector extends AppCompatActivity {
 
 
     public void addNewCode(View view) {
-        final int result = 1;
         Intent codeEditorIntent = new Intent(this,CodeEditor.class);
         codeEditorIntent.putExtra("idSelected",(String) null);
-        this.startActivityForResult(codeEditorIntent,result);
+        this.startActivityForResult(codeEditorIntent,OPEN_WRITER);
     }
 
     public static void createFolder(){
@@ -142,13 +171,14 @@ public class selector extends AppCompatActivity {
                 break;
             }
             case R.id.add_file:{
-
+                    Intent intent = new Intent(Intent.ACTION_OPEN_DOCUMENT);
+                    intent.addCategory(Intent.CATEGORY_OPENABLE);
+                    intent.setType("text/*");
+                    startActivityForResult(intent, FIND_FILE);
                 break;
             }
             // case blocks for other MenuItems (if any)
         }
         return false;
     }
-
-
 }
