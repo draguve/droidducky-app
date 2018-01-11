fe9085f20b6c6fff268efb6de9416bcefa6a99e8
Draguve <ritwikpatil1998@gmail.com>
2018-01-12 00:51:58 +0530

    Fixed some server
    still untested

--- app/src/main/java/com/draguve/droidducky/httpserver.java
+++ app/src/main/java/com/draguve/droidducky/httpserver.java
@@ -2,6 +2,7 @@ package com.draguve.droidducky;
 
 import android.os.Environment;
 
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.util.Map;
@@ -19,19 +20,16 @@ public class httpserver extends NanoHTTPD {
     }
 
     @Override
-    public Response serve(String uri, Method method,
-                          Map<String, String> header, Map<String, String> parameters,
-                          Map<String, String> files) {
+    public Response serve(String uri, Method method, Map<String, String> header, Map<String, String> parameters, Map<String, String> files) {
         String answer = "";
-
         FileInputStream fis = null;
         try {
-            fis = new FileInputStream(Environment.getExternalStorageDirectory()
-                    + "/music/musicfile.mp3");
+            fis = new FileInputStream(Environment.getExternalStorageDirectory() + "/Droidducky/draguve.txt");
         } catch (FileNotFoundException e) {
             // TODO Auto-generated catch block
             e.printStackTrace();
         }
-        return new NanoHTTPD.Response(Response.Status.OK, "audio/mpeg", fis);
+        //return new NanoHTTPD.Response(Response.Status.OK, "audio/mpeg", fis);
+        return NanoHTTPD.newChunkedResponse(Response.Status.OK,"text/plain",fis);
     }
 }
